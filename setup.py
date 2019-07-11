02df062ffc52c45aba17405022a3032baa19394d
John Andersen <johnandersenpdx@gmail.com>
2019-07-11 12:03:33 -0700

    cli: randstr
    Signed-off-by: John Andersen <johnandersenpdx@gmail.com>

--- setup.py
+++ setup.py
@@ -3,26 +3,23 @@ import ast
 from io import open
 from setuptools import find_packages, setup
 
-AUTHOR_NAME = 'John Andersen'
-AUTHOR_EMAIL = 'johnandersenpdx@gmail.com'
-ORG = 'pdxjohnny'
-NAME = 'twothings'
-DESCRIPTION = 'You want me to do two things?'
-INSTALL_REQUIRES = [
-    'keyring>=19.0.1',
-    'exchangelib[kerberos]>=1.12.3',
-    ]
+AUTHOR_NAME = "John Andersen"
+AUTHOR_EMAIL = "johnandersenpdx@gmail.com"
+ORG = "pdxjohnny"
+NAME = "twothings"
+DESCRIPTION = "You want me to do two things?"
+INSTALL_REQUIRES = ["dffml>=0.2.1", "keyring>=19.0.1", "exchangelib[kerberos]>=1.12.3"]
 
-IMPORT_NAME = NAME.replace('-', '_')
+IMPORT_NAME = NAME.replace("-", "_")
 SELF_PATH = os.path.dirname(os.path.realpath(__file__))
 
-with open(os.path.join(SELF_PATH, IMPORT_NAME, 'version.py'), 'r') as f:
+with open(os.path.join(SELF_PATH, IMPORT_NAME, "version.py"), "r") as f:
     for line in f:
-        if line.startswith('VERSION'):
-            version = ast.literal_eval(line.strip().split('=')[-1].strip())
+        if line.startswith("VERSION"):
+            version = ast.literal_eval(line.strip().split("=")[-1].strip())
             break
 
-with open(os.path.join(SELF_PATH, 'README.md'), 'r', encoding='utf-8') as f:
+with open(os.path.join(SELF_PATH, "README.md"), "r", encoding="utf-8") as f:
     readme = f.read()
 
 setup(
@@ -30,32 +27,26 @@ setup(
     version=version,
     description=DESCRIPTION,
     long_description=readme,
-    long_description_content_type='text/markdown',
+    long_description_content_type="text/markdown",
     author=AUTHOR_NAME,
     author_email=AUTHOR_EMAIL,
     maintainer=AUTHOR_NAME,
     maintainer_email=AUTHOR_EMAIL,
-    url='https://github.com/' + ORG + '/' + NAME,
-    license='MIT',
-    keywords=[
-        '',
-    ],
+    url="https://github.com/" + ORG + "/" + NAME,
+    license="MIT",
+    keywords=[""],
     classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Natural Language :: English',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python :: 3 :: Only',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy',
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
     ],
     packages=find_packages(),
     install_requires=INSTALL_REQUIRES,
-    entry_points={
-        'console_scripts': [
-            # NAME + ' = ' + IMPORT_NAME + '.calendar.sync:CalendarSync.cli',
-        ],
-    },
+    entry_points={"console_scripts": ["twothings = twothings.cli:CLI.main"]},
 )
---
+++ twothings/cli.py
@@ -0,0 +1,54 @@
+import os
+import hashlib
+
+from dffml.util.cli.arg import Arg
+from dffml.util.cli.cmd import CMD
+
+RAND_STR_DEFAULT_BITS_ENTROPY = 4096
+
+class RandStr(CMD):
+    """
+    Generate a random string
+    """
+
+    arg_bits = Arg('--bits', default=RAND_STR_DEFAULT_BITS_ENTROPY, type=int, help=f"Number of bits of entropy (default: {RAND_STR_DEFAULT_BITS_ENTROPY})")
+
+    async def run(self):
+        print(hashlib.sha384(os.urandom(int(self.bits / 8))).hexdigest())
+
+class CLI(CMD):
+    '''
+     __   __                                _                      _
+     \\ \\ / /__  _   _  __      ____ _ _ __ | |_   _ __ ___   ___  | |_ ___
+      \\ V / _ \\| | | | \\ \\ /\\ / / _` | '_ \\| __| | '_ ` _ \\ / _ \\ | __/ _ \\
+       | | (_) | |_| |  \\ V  V / (_| | | | | |_  | | | | | |  __/ | || (_) |
+       |_|\\___/ \\__,_|   \\_/\\_/ \\__,_|_| |_|\\__| |_| |_| |_|\\___|  \\__\\___/
+
+			     _
+			    ( )
+			     H
+			     H
+			    _H_
+			 .-'-.-'-.
+			/         \\
+		       |           |
+		       |   .-------'._
+		       |  / /  '.' '. \\
+		       |  \\ \\ @   @ / /
+		       |   '---------'
+		       |    _______|
+		       |  .'-+-+-+|
+		       |  '.-+-+-+|
+		       |    """""" |
+		       '-.__   __.-'
+			    """
+
+	  _         _                   _   _     _                ___
+       __| | ___   | |___      _____   | |_| |__ (_)_ __   __ _ __|__ \\
+      / _` |/ _ \\  | __\\ \\ /\\ / / _ \\  | __| '_ \\| | '_ \\ / _` / __|/ /
+     | (_| | (_) | | |_ \\ V  V / (_) | | |_| | | | | | | | (_| \\__ \\_|
+      \\__,_|\\___/   \\__| \\_/\\_/ \\___/   \\__|_| |_|_|_| |_|\\__, |___(_)
+							  |___/
+    '''
+
+    randstr = RandStr
